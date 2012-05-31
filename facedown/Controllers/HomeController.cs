8e34807f85d8486e367e94db957e1a60b3d5eccf
Agustin Recouso <agustinrecouso@fiqus.com>
2012-05-30 22:08:24 -0300

    Archivos del Proyecto. Set up.

---
+++ facedown/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace facedown.Controllers
+{
+    [HandleError]
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            Session["user"] = FB.GetUser("me");
+
+            ViewData["Message"] = "Welcome to ASP.NET MVC!";
+
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            return View();
+        }
+    }
+}
